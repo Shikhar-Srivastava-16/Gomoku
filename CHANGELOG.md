286f8ccf675f0044d896ddf2783bd01f845ad978
Shikhar Srivastava <ss504@st-andrews.ac.uk>
2025-04-03 14:58:56 +0100

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for Gomoku
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ dist-newstyle/build/x86_64-linux/ghc-8.10.7/Gomoku-0.1.0.0/x/Gomoku/build/Gomoku/autogen/Paths_Gomoku.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+module Paths_Gomoku (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/home/ss504/.cabal/bin"
+libdir     = "/home/ss504/.cabal/lib/x86_64-linux-ghc-8.10.7/Gomoku-0.1.0.0-inplace-Gomoku"
+dynlibdir  = "/home/ss504/.cabal/lib/x86_64-linux-ghc-8.10.7"
+datadir    = "/home/ss504/.cabal/share/x86_64-linux-ghc-8.10.7/Gomoku-0.1.0.0"
+libexecdir = "/home/ss504/.cabal/libexec/x86_64-linux-ghc-8.10.7/Gomoku-0.1.0.0"
+sysconfdir = "/home/ss504/.cabal/etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "Gomoku_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "Gomoku_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "Gomoku_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "Gomoku_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "Gomoku_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "Gomoku_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
